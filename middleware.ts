8eaeecae642e8948e75c948442b2ef42d1042588
xxxitachi <sayedsakib@nextventures.io>
2026-06-15 12:52:20 +0600

    Delete middleware.ts

--- middleware.ts
+++
@@ -1,7 +0,0 @@
-export { default } from "next-auth/middleware";
-
-export const config = {
-  // Protect every route under /dashboard
-  // Unauthenticated users are automatically redirected to the sign-in page
-  matcher: ["/dashboard/:path*"],
-};
